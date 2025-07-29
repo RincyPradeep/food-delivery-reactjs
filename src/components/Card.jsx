42c02413d050974607af429a3dec5f448d5bac07
Rincy Pradeep <rincypradeep15@gmail.com>
2025-07-29 20:07:54 +0530

    food items list and filter

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import image1 from "../assets/image1.avif"
 import { LuLeafyGreen } from "react-icons/lu";
+import { GiChickenOven } from "react-icons/gi";
 
-const Card = () => {
+
+const Card = ({name,image,id,price,type}) => {
   return (
-    <div className='w-[300px] h-[400px] bg-white p-4 rounded-lg flex flex-col gap-3'>
+    <div className='w-[300px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3 shadow-lg
+      hover:border-2 border-green-300'>
         <div className='w-[100%] h-[60%] overflow-hidden rounded-lg'>
-            <img src={image1} alt="" className='object-cover'/>
+            <img src={image} alt="" className='object-cover'/>
         </div>
-        <div className='text-2xl font-semibold'>Pancacke</div>
-        <div>299/-</div>
-        <div>
-            <LuLeafyGreen />
-            <span>Veg</span>
+        <div className='text-2xl font-semibold'>{name}</div>
+        <div className='w-full flex justify-between items-center'>
+          <div className='text-lg font-bold text-green-500'>Rs. {price}/-</div>
+          <div className='flex justify-center items-center gap-2 text-lg text-green-500 font-semibold'>
+              {type==="veg" ?<LuLeafyGreen /> : <GiChickenOven />}
+              <span>{type}</span>
+          </div>
         </div>
-
+        <button className='p-3 bg-green-400 text-white w-full rounded-lg hover:bg-green-500 transition-all'>Add to dish</button>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from '../components/Nav'
 import Categories from '../Category'
 import Card from '../components/Card'
+import {food_items} from '../food'
 
 const Home = () => {
+  const [filteredItems, setFilteredItems] = useState(food_items)
+
+  const handleFilter = (category)=>{
+    if(category==='All')
+      setFilteredItems(food_items)
+    else
+      setFilteredItems(food_items.filter(item=>item.food_category===category))
+  }
   return (
     <div className='w-full bg-slate-200 min-h-screen'>
         <Nav />
@@ -12,14 +21,19 @@ const Home = () => {
             Categories.map(category=>{
               return <div key ={category.id} className='w-[140px] h-[150px] bg-white flex flex-col justify-start items-start 
               gap-5 p-5 text-[20px] font-semibold text-gray-600 rounded-lg shadow-xl hover:bg-green-200
-              cursor-pointer transition-all duration-500'>
+              cursor-pointer transition-all duration-500'
+              onClick={()=>handleFilter(category.name)}>
               {category.icon}
               {category.name}
               </div>
             })
           }
         </div>
-        <Card />
+        <div className='w-full flex flex-wrap px-5 gap-5 justify-center items-center py-8'>
+          {filteredItems.map(item=>(
+            <Card name={item.food_name} image={item.food_image} id={item.id} price={item.price} type={item.food_type} />
+          ))}
+        </div>
     </div>
   )
 }
